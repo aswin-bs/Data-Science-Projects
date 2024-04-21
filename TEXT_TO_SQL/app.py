05df5be28d32cd39e77ced3c7a929be01d0b4c6a
aswin-bs <aswinbs1452@gmail.com>
2024-04-21 15:56:57 +0530

    small change

--- TEXT_TO_SQL/app.py
+++ TEXT_TO_SQL/app.py
@@ -10,6 +10,7 @@ import google.generativeai as genai
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
+#write gemini function
 def get_gemini_response(question,prompt):
     model=genai.GenerativeModel('gemini-pro')
     response=model.generate_content([prompt[0],question])

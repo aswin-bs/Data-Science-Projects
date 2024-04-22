2b7c7c15b79f49f590654fa5a5cad9fef71b9f34
aswin-bs <aswinbs1452@gmail.com>
2024-04-23 00:06:20 +0530

    .

--- TEXT_TO_SQL/sql.py
+++ TEXT_TO_SQL/sql.py
@@ -5,12 +5,14 @@ connection = sqlite3.connect("student.db")
 
 cursor = connection.cursor()
 
-table_info="""
-Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
-SECTION VARCHAR(25),MARKS INT);
 
-"""
-cursor.execute(table_info)
+#table_info="""
+#Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+#SECTION VARCHAR(25),MARKS INT);"""
+
+
+
+#cursor.execute(table_info)
 
 cursor.execute('''Insert Into STUDENT values('Balanarayanan','Data Science','A',90)''')
 cursor.execute('''Insert Into STUDENT values('Riswi R','Data Science','B',100)''')

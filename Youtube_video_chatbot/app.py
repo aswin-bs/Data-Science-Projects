f24a71c0c8bc897a44b8ee0eabc1b90e89a2fb9b
aswin-bs <aswinbs1452@gmail.com>
2024-04-25 22:18:54 +0530

    created a youtubee qna chatbot

---
+++ Youtube_video_chatbot/app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+
+load_dotenv()
+import os
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt="""You are a YouTube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
+
+prompt_2="""You are a YouTube video question answering chat bot . You are provided with
+youtube transcript . Please answer the following question based on the transcipt text provided """
+
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+        
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+    except Exception as e:
+        raise e
+
+def generate_gemini_content(transcript_text,prompt):
+
+    model=genai.GenerativeModel("gemini-pro")
+    response=model.generate_content(prompt+transcript_text)
+    return response.text
+
+def generate_gemini_qna(transcript_text,prompt,question):
+
+    model=genai.GenerativeModel("gemini-pro")
+    response=model.generate_content(prompt+transcript_text+question)
+    return response.text
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    print(video_id)
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Detailed Notes"):
+    transcript_text=extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary=generate_gemini_content(transcript_text,prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
+
+if st.button("Ask Question"):
+    question = st.text_input("Enter your question:")
+    transcript_text=extract_transcript_details(youtube_link)
+    if question:
+        response = generate_gemini_qna(prompt_2,transcript_text, question)
+        st.markdown("## Response:")
+        st.write(response)
